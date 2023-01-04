065f738d7195e79e1028e6e6560dc9b89bc8d948
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2023-01-04 22:14:02 +0530

    Add files via upload

---
+++ ml.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+df=pd.read_csv("class_room-216-217.csv")
+x=df.drop("place",axis=1)
+y=df.place
+#x_train,x_test,y_train,y_test=train_test_split(features, labels, test_size=0.2, random_state=42)
+knn = KNeighborsClassifier(n_neighbors=19,metric='euclidean')
+knn.fit(x,y)
+print(knn.predict([[13.6300942,78.477928,216],])[0])
+pickle.dump(knn,open("model.pkl","wb"))
+
+
+
